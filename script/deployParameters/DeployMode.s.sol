a657934fc83e7db346955f0f886fde49d5d37445
simplyoptimistic <111120814+simplyoptimistic@users.noreply.github.com>
2024-05-16 11:27:33 +1000

    feat: mode deployment (#25)

--- script/deployParameters/DeployMode.s.sol
+++ script/deployParameters/DeployMode.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.15;
 
-import {DeployBase} from "../01_DeployBase.s.sol";
+import "../01_DeployBase.s.sol";
 import {ModePool} from "src/pools/extensions/ModePool.sol";
 import {ModePoolFactory} from "src/pools/extensions/ModePoolFactory.sol";
 import {ModeRouter} from "src/extensions/ModeRouter.sol";
@@ -20,30 +20,44 @@ contract DeployMode is DeployBase {
     }
 
     ModeDeploymentParameters internal _modeParams;
-    address[] public whitelistedTokens = new address[](1);
+    address[] public whitelistedTokens = new address[](15);
 
     ModeStakingRewardsFactory public stakingRewardsFactory;
     ModeStakingRewards public stakingRewards;
 
     function setUp() public override {
         whitelistedTokens.push(0x4200000000000000000000000000000000000006);
+        whitelistedTokens.push(0xDfc7C877a950e49D2610114102175A06C2e3167a);
+        whitelistedTokens.push(0x59889b7021243dB5B1e065385F918316cD90D46c);
+        whitelistedTokens.push(0x4186BFC76E2E237523CBC30FD220FE055156b41F);
+        whitelistedTokens.push(0xe7903B1F75C534Dd8159b313d92cDCfbC62cB3Cd);
+        whitelistedTokens.push(0x2416092f143378750bb29b79eD961ab195CcEea5);
+        whitelistedTokens.push(0x80137510979822322193FC997d400D5A6C747bf7);
+        whitelistedTokens.push(0xd988097fb8612cc24eeC14542bC03424c656005f);
+        whitelistedTokens.push(0xf0F161fDA2712DB8b566946122a5af183995e2eD);
+        whitelistedTokens.push(0x04C0599Ae5A44757c0af6F9eC3b93da8976c150A);
+        whitelistedTokens.push(0xcDd475325D6F564d27247D1DddBb0DAc6fA0a5CF);
+        whitelistedTokens.push(0x9e5AAC1Ba1a2e6aEd6b32689DFcF62A509Ca96f3);
+        whitelistedTokens.push(0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb);
+        whitelistedTokens.push(0xE7798f023fC62146e8Aa1b36Da45fb70855a77Ea);
+        whitelistedTokens.push(0x18470019bF0E94611f15852F7e93cf5D65BC34CA);
 
         _params = DeployBase.DeploymentParameters({
             weth: 0x4200000000000000000000000000000000000006,
-            poolAdmin: 0x0000000000000000000000000000000000000001,
-            pauser: 0x0000000000000000000000000000000000000001,
-            feeManager: 0x0000000000000000000000000000000000000001,
-            whitelistAdmin: 0x0000000000000000000000000000000000000001,
+            poolAdmin: 0xA6074AcC04DeAb343881882c896555A1Ba2E9d46,
+            pauser: 0xA6074AcC04DeAb343881882c896555A1Ba2E9d46,
+            feeManager: 0xA6074AcC04DeAb343881882c896555A1Ba2E9d46,
+            whitelistAdmin: 0xA6074AcC04DeAb343881882c896555A1Ba2E9d46,
             whitelistedTokens: whitelistedTokens,
             outputFilename: "Mode.json"
         });
         _modeParams = ModeDeploymentParameters({
             sfs: 0x8680CEaBcb9b56913c519c069Add6Bc3494B7020,
-            recipient: 0x0000000000000000000000000000000000000001,
-            keeperAdmin: 0x0000000000000000000000000000000000000001,
-            notifyAdmin: 0x0000000000000000000000000000000000000001,
-            admin: 0x0000000000000000000000000000000000000001,
-            rewardToken: 0x0000000000000000000000000000000000000001
+            recipient: 0xb8804281fc224a4E597A3f256b53C9Ed3C89B6c3,
+            keeperAdmin: 0xb32Db5b848B837DC39EF20B4110dFAc7493e93ed,
+            notifyAdmin: 0xA6074AcC04DeAb343881882c896555A1Ba2E9d46,
+            admin: 0x5d496974832B8BC3c02F89E7Ac0b7579b4d1cC09,
+            rewardToken: 0xDfc7C877a950e49D2610114102175A06C2e3167a
         });
     }
 
@@ -107,6 +121,9 @@ contract DeployMode is DeployBase {
             _recipient: _modeParams.recipient,
             _keepers: new address[](0)
         });
+
+        console2.log("TokenRegistry deployed at: ", address(tokenRegistry));
+        console2.log("StakingRewardsFactory deployed at: ", address(stakingRewardsFactory));
     }
 
     function modeParams() public view returns (ModeDeploymentParameters memory) {
