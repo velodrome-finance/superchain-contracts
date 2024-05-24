d3f16b568cfc9d44fe7755a1b7dba7a56a4d349e
Pedro Valido <80268365+pedrovalido@users.noreply.github.com>
2024-05-24 18:58:50 +0100

    test: pool factory (#52)
    * test: pool factory
    
    * refactor: adapt current pool factory tests to btt

---
+++ test/unit/pool/PoolFactory/setFeeManager/setFeeManager.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity >=0.8.19 <0.9.0;
+
+import "../../../../BaseFixture.sol";
+
+contract SetFeeManagerTest is BaseFixture {
+    function test_WhenCallerIsNotFeeManager() external {
+        // It should revert with NotFeeManager
+        vm.prank(users.charlie);
+        vm.expectRevert(IPoolFactory.NotFeeManager.selector);
+        poolFactory.setFeeManager(users.owner);
+    }
+
+    modifier whenCallerIsFeeManager() {
+        vm.startPrank(poolFactory.feeManager());
+        _;
+        vm.stopPrank();
+    }
+
+    function test_WhenTheFeeManagerIsAddressZero() external whenCallerIsFeeManager {
+        // It should revert with ZeroAddress
+        vm.expectRevert(IPoolFactory.ZeroAddress.selector);
+        poolFactory.setFeeManager(address(0));
+    }
+
+    function test_WhenTheFeeManagerIsNotAddressZero() external whenCallerIsFeeManager {
+        // It should set the new fee manager
+        // It should emit a {SetFeeManager} event
+        assertNotEq(poolFactory.feeManager(), users.alice);
+        vm.expectEmit(address(poolFactory));
+        emit IPoolFactory.SetFeeManager({feeManager: users.alice});
+        poolFactory.setFeeManager(users.alice);
+        assertEq(poolFactory.feeManager(), users.alice);
+    }
+}
