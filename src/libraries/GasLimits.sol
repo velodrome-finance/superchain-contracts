6db7550e714d1e865430159406442fa7cdc50a89
simplyoptimistic <111120814+simplyoptimistic@users.noreply.github.com>
2024-10-30 16:36:58 +0900

    chore: bump gas limits (#172)

--- src/libraries/GasLimits.sol
+++ src/libraries/GasLimits.sol
@@ -10,10 +10,10 @@ library GasLimits {
     /// @dev Returns 0 if invalid _command
     function gasLimit(uint256 _command) internal pure returns (uint256) {
         if (_command == Commands.DEPOSIT) return 530_000;
-        if (_command == Commands.WITHDRAW) return 145_000;
+        if (_command == Commands.WITHDRAW) return 125_000;
         if (_command == Commands.GET_INCENTIVES) return 710_000;
         if (_command == Commands.GET_FEES) return 325_000;
-        if (_command == Commands.CREATE_GAUGE) return 5_800_000;
+        if (_command == Commands.CREATE_GAUGE) return 7_320_000;
         if (_command == Commands.NOTIFY) return 282_000;
         if (_command == Commands.NOTIFY_WITHOUT_CLAIM) return 254_000;
         if (_command == Commands.KILL_GAUGE) return 90_000;
