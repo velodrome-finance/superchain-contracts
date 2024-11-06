e514c92b0925cdb5e11ceb8ccc8a272f3fc0da33
simplyoptimistic <111120814+simplyoptimistic@users.noreply.github.com>
2024-11-06 20:46:13 +0900

    chore: bump gas limits (#191)

--- src/libraries/GasLimits.sol
+++ src/libraries/GasLimits.sol
@@ -9,15 +9,15 @@ import {Commands} from "./Commands.sol";
 library GasLimits {
     /// @dev Returns 0 if invalid _command
     function gasLimit(uint256 _command) internal pure returns (uint256) {
-        if (_command == Commands.DEPOSIT) return 530_000;
-        if (_command == Commands.WITHDRAW) return 125_000;
-        if (_command == Commands.GET_INCENTIVES) return 710_000;
-        if (_command == Commands.GET_FEES) return 325_000;
-        if (_command == Commands.CREATE_GAUGE) return 7_320_000;
-        if (_command == Commands.NOTIFY) return 282_000;
-        if (_command == Commands.NOTIFY_WITHOUT_CLAIM) return 254_000;
-        if (_command == Commands.KILL_GAUGE) return 90_000;
-        if (_command == Commands.REVIVE_GAUGE) return 187_000;
+        if (_command == Commands.DEPOSIT) return 281_000;
+        if (_command == Commands.WITHDRAW) return 75_000;
+        if (_command == Commands.GET_INCENTIVES) return 650_000;
+        if (_command == Commands.GET_FEES) return 300_000;
+        if (_command == Commands.CREATE_GAUGE) return 6_710_000;
+        if (_command == Commands.NOTIFY) return 280_000;
+        if (_command == Commands.NOTIFY_WITHOUT_CLAIM) return 233_000;
+        if (_command == Commands.KILL_GAUGE) return 83_000;
+        if (_command == Commands.REVIVE_GAUGE) return 169_000;
         return 0;
     }
 }
